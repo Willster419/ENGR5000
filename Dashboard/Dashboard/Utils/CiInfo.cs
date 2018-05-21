62c6179a8196715379ed43efb82851f54098f129
Willster419 <willster419@outlook.com>
2018-05-20 22:46:27 -0400

    Added build info parameters

---
+++ Dashboard/Dashboard/Utils/CiInfo.cs
@@ -0,0 +1,9 @@
+namespace RelhaxModpack 
+{ 
+    ///<summary>Info about the continuous integration server build that produced this binary.</summary> 
+    public static class CiInfo 
+    { 
+        ///<summary>String of the build number and build date/time, and other useful info.</summary> 
+        public const string BuildTag = "Sun 05/20/2018 22:45:13.28"; 
+    } 
+} 
