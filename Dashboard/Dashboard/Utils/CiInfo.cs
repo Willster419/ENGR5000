7bb3f0a9fe224ca76013c8cac7f4822f10cfa348
Willster419 <willster419@outlook.com>
2018-05-21 14:58:17 -0400

    ignore this file plz

--- Dashboard/Dashboard/Utils/CiInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RelhaxModpack 
-{ 
-    ///<summary>Info about the continuous integration server build that produced this binary.</summary> 
-    public static class CiInfo 
-    { 
-        ///<summary>String of the build number and build date/time, and other useful info.</summary> 
-        public const string BuildTag = "Mon 05/21/2018 11:14:13.62"; 
-    } 
-} 
