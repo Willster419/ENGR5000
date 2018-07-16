eb53efff8eb226d635f15e4dd4a5c3bbd7873df6
Willster419 <willster419@outlook.com>
2018-07-16 17:21:28 -0400

    I2C device works

---
+++ DemoProjects/Accelerometer_MPU6050-master/Accelerometer_MPU6050/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MPU6050;
+
+namespace Accelerometer_MPU6050
+{
+    public sealed partial class MainPage : Page
+    {
+        readonly Mpu6050 _mpu6050 = new Mpu6050();
+        int _interruptCount = 0;
+        readonly DateTime _startTime;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _mpu6050.InitHardware();
+            _mpu6050.SensorInterruptEvent += _mpu6050_SensorInterruptEvent;
+            _startTime = DateTime.Now;
+        }
+
+        private void _mpu6050_SensorInterruptEvent(object sender, MpuSensorEventArgs e)
+        {
+            var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                _interruptCount += e.Values.Length;
+                float samplesPerSecond = (float) _interruptCount/(float) ((DateTime.Now - _startTime).Seconds);
+
+                textBoxStatus.Text = String.Format("{0} {1} {2}", e.Status, e.SamplePeriod, samplesPerSecond);
+                textBoxAccel.Text = String.Format("{0}, {1}, {2}", e.Values[0].AccelerationX, e.Values[0].AccelerationY,
+                    e.Values[0].AccelerationZ);
+                textBoxGyro.Text = String.Format("{0}, {1}, {2}", e.Values[0].GyroX, e.Values[0].GyroY,
+                    e.Values[0].GyroZ);
+            });
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            _mpu6050.Dispose();
+            Application.Current.Exit();
+        }
+    }
+}
--- DemoProjects/I2C_test/I2C_test/MainPage.xaml.cs
+++ DemoProjects/I2C_test/I2C_test/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using Windows.Devices.I2c;
 using Windows.Devices.Gpio;
 using Microsoft.IoT.Lightning;
 using Microsoft.IoT.Lightning.Providers;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,6 +29,25 @@ namespace I2C_test
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Constants
+        public const byte ADDRESS = 0x68;
+        private const byte PWR_MGMT_1 = 0x6B;
+        private const byte SMPLRT_DIV = 0x19;
+        private const byte CONFIG = 0x1A;
+        private const byte GYRO_CONFIG = 0x1B;
+        private const byte ACCEL_CONFIG = 0x1C;
+        private const byte FIFO_EN = 0x23;
+        private const byte INT_ENABLE = 0x38;
+        private const byte INT_STATUS = 0x3A;
+        private const byte USER_CTRL = 0x6A;
+        private const byte FIFO_COUNT = 0x72;
+        private const byte FIFO_R_W = 0x74;
+
+        private const int SensorBytes = 12;
+        #endregion
+        private const Int32 INTERRUPT_PIN = 21;
+        private GpioController _gpioController;
+        private GpioPin _intPin;
         private I2cController _i2CController;
         private I2cConnectionSettings _i2CConnectionSettings;
         private I2cDevice _i2CDevice;
@@ -47,8 +67,111 @@ namespace I2C_test
             {
                 return;
             }
+            //https://github.com/Microsoft/Windows-universal-samples/blob/master/Samples/IoT-I2C/cs/Scenario1_ReadData.xaml.cs
+            _gpioController = GpioController.GetDefault();
+            _intPin = _gpioController.OpenPin(INTERRUPT_PIN);
+            _intPin.Write(GpioPinValue.Low);
+            _intPin.SetDriveMode(GpioPinDriveMode.Input);
+            _intPin.ValueChanged += _dispatcherTimer_Tick;
             _i2CController = await I2cController.GetDefaultAsync();
-            //TODO:
+            if (_i2CController == null)
+                return;
+            //TODO: make constants for device names
+            _i2CConnectionSettings = new I2cConnectionSettings(0x68)//MPU-6050 address
+            {
+                BusSpeed = I2cBusSpeed.FastMode,
+            };
+            _i2CDevice = _i2CController.GetDevice(_i2CConnectionSettings);
+            if (_i2CDevice == null)
+                return;
+            await Task.Delay(3); // wait power up sequence
+
+            WriteByte(PWR_MGMT_1, 0x80);// reset the device
+            await Task.Delay(100);
+            WriteByte(PWR_MGMT_1, 0x2);
+            WriteByte(USER_CTRL, 0x04); //reset fifo
+
+            WriteByte(PWR_MGMT_1, 1); // clock source = gyro x
+            WriteByte(GYRO_CONFIG, 0); // +/- 250 degrees sec
+            WriteByte(ACCEL_CONFIG, 0); // +/- 2g
+
+            WriteByte(CONFIG, 1); // 184 Hz, 2ms delay
+            WriteByte(SMPLRT_DIV, 19);  // set rate 50Hz
+            WriteByte(FIFO_EN, 0x78); // enable accel and gyro to read into fifo
+            WriteByte(USER_CTRL, 0x40); // reset and enable fifo
+            WriteByte(INT_ENABLE, 0x1);
+        }
+
+        private void _dispatcherTimer_Tick(GpioPin sender, GpioPinValueChangedEventArgs e)
+        {
+            int interruptStatus = ReadByte(INT_STATUS);
+            if ((interruptStatus & 0x10) != 0)
+            {
+                WriteByte(USER_CTRL, 0x44); // reset and enable fifo
+            }
+            if ((interruptStatus & 0x1) == 0) return;
+            int count = ReadWord(FIFO_COUNT);
+            while (count >= SensorBytes)
+            {
+                var data = ReadBytes(FIFO_R_W, SensorBytes);
+                count -= SensorBytes;
+
+                var xa = (short)(data[0] << 8 | data[1]);
+                var ya = (short)(data[2] << 8 | data[3]);
+                var za = (short)(data[4] << 8 | data[5]);
+
+                var xg = (short)(data[6] << 8 | data[7]);
+                var yg = (short)(data[8] << 8 | data[9]);
+                var zg = (short)(data[10] << 8 | data[11]);
+
+
+                float AccelerationX = xa / (float)16384;
+                float AccelerationY = ya / (float)16384;
+                float AccelerationZ = za / (float)16384;
+                float GyroX = xg / (float)131;
+                float GyroY = yg / (float)131;
+                float GyroZ = zg / (float)131;
+
+
+                AccelDataX.Text = string.Format("AccelX = {0}", AccelerationX);
+                AccelDataY.Text = string.Format("AccelY = {0}", AccelerationY);
+                AccelDataZ.Text = string.Format("AccelZ = {0}", AccelerationZ);
+                GyroDataX.Text = string.Format("GyroX = {0}", GyroX);
+                GyroDataY.Text = string.Format("GyroY = {0}", GyroY);
+                GyroDataZ.Text = string.Format("GryoZ = {0}", GyroZ);
+            }
+        }
+        //https://www.hackster.io/graham_chow/mpu-6050-for-windows-iot-d67793
+        void WriteByte(byte regAddr, byte data)
+        {
+            byte[] buffer = new byte[2];
+            buffer[0] = regAddr;
+            buffer[1] = data;
+            _i2CDevice.Write(buffer);
+        }
+
+        private byte ReadByte(byte regAddr)
+        {
+            byte[] buffer = new byte[1];
+            buffer[0] = regAddr;
+            byte[] value = new byte[1];
+            _i2CDevice.WriteRead(buffer, value);
+            return value[0];
+        }
+
+        private byte[] ReadBytes(byte regAddr, int length)
+        {
+            byte[] values = new byte[length];
+            byte[] buffer = new byte[1];
+            buffer[0] = regAddr;
+            _i2CDevice.WriteRead(buffer, values);
+            return values;
+        }
+
+        public ushort ReadWord(byte address)
+        {
+            byte[] buffer = ReadBytes(FIFO_COUNT, 2);
+            return (ushort)(((int)buffer[0] << 8) | (int)buffer[1]);
         }
     }
 }
